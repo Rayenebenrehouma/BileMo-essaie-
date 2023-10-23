d3e7e6efc32175aa194599d07e154b146037b0d6
Rayen <rayenebenrehouma@outlook.fr>
2023-10-23 10:19:36 +0200

    Sécurité JWT

--- src/Controller/PhoneController.php
+++ src/Controller/PhoneController.php
@@ -2,11 +2,15 @@
 
 namespace App\Controller;
 
+use App\Entity\Phone;
 use App\Repository\PhoneRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use http\Env\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 use Symfony\Component\Serializer\SerializerInterface;
 
 class PhoneController extends AbstractController
@@ -19,5 +23,29 @@ class PhoneController extends AbstractController
         //dd($phoneList);
 
         return new JsonResponse($jsonPhoneList, Response::HTTP_OK, [], true);
+    }
+
+    //Détails des téléphone
+    #[Route('/api/phones/{id}', name: 'phoneDetails', methods: ['GET'])]
+    public function getPhoneDetails(SerializerInterface $serializer, Phone $phone)
+    {
+            $jsonPhone = $serializer->serialize($phone, 'json');
+            return new JsonResponse($jsonPhone, Response::HTTP_OK, [], true);
+    }
+
+
+    #[Route('/api/phones/{id}', name: 'deletePhone', methods: ['DELETE'])]
+    public function deletePhone(Phone $phone, EntityManagerInterface $em): JsonResponse
+    {
+
+        $em->remove($phone);
+        $em->flush();
+
+        return new JsonResponse(null, Response::HTTP_NO_CONTENT);
+    }
+
+    public function createPhone(Request $request,SerializerInterface $serializer,EntityManagerInterface $em, UrlGeneratorInterface $urlGenerator): JsonResponse
+    {
+
     }
 }
---
+++ src/Form/PhoneType.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Phone;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class PhoneType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('brand')
+            ->add('color')
+            ->add('model')
+            ->add('description')
+            ->add('price')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Phone::class,
+        ]);
+    }
+}
